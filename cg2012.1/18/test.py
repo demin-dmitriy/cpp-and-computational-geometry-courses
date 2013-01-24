f4505bf482e898508661e2cbe7cdf1174ff2a852
ddf.fez@gmail.com <ddf.fez@gmail.com@68c717b3-5d72-a4ee-e31f-601cb0525dea>
2013-01-24 18:00:08 +0000

    Prepaired for 18 task. Made statement, checker, cgal-solution and visualizer. Using tests from 01 task

---
+++ cg2012.1/18/test.py
@@ -0,0 +1,58 @@
+from subprocess import call
+from time import perf_counter
+
+from os import chdir
+from sys import argv
+chdir("..\\18-build")
+
+tested_solution = "voronoi_diagram.exe"
+correct_solution = "voronoi_diagram_cgal.exe"
+checker = "voronoi_diagram_checker.exe"
+
+tmp_file1 = "res1.out"
+tmp_file2 = "res2.out"
+
+def run_solution(solution, input_file, output_file):
+    file_in = open(input_file)
+    file_out = open(output_file, "w")
+    t1 = perf_counter()
+    res = call([solution], stdin=file_in, stdout=file_out)
+    t2 = perf_counter()
+    if res != 0:
+        print("    Error: program returned non-zero.")
+        return
+    file_out.close()
+    file_in.close()
+    return t2 - t1
+
+def test(file):
+    t = run_solution(correct_solution, file, tmp_file1)
+    print("    Cgal solution completed in {}.".format(t))
+    t = run_solution(tested_solution, file, tmp_file2)
+    print("    Solution completed in {}.".format(t))
+    res = call([checker, tmp_file1, tmp_file2])
+    if res == 0:
+        print("    Correct.")
+    else:
+        print("    Wrong answer.")
+
+if __name__ == "__main__":
+    if len(argv) >= 2:
+        prefix = "correctness_tests"
+        if len(argv) >= 3:
+            if argv[1] == "p":
+                prefix = "performance_tests"
+            i = int(argv[2])
+        else:
+            i = int(argv[1])
+        test("{}\\{}.in".format(prefix, str(i).zfill(3)))
+    else:
+        print("Correctness tests:")
+        for i in range(48):
+            print("test {}:".format(i))
+            test("correctness_tests\\{}.in".format(str(i).zfill(3)))
+        print("Performance tests:")
+        for i in range(3):
+            print("test {}:".format(i))
+            test("performance_tests\\{}.in".format(str(i).zfill(3)))
+    
